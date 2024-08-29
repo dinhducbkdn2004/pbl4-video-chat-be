edcbcec75200a9308419f6541cade37af38184b8
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2024-08-29 09:28:57 +0700

    feat: add validation login + register

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,69 +1,59 @@
 import mongoose from "mongoose";
 import modelOption from "../configs/model.config";
 
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    avatar: {
-        type: String,
-        required: true,
-        default:
-            "https://cellphones.com.vn/sforum/wp-content/uploads/2023/10/avatar-trang-4.jpg",
-    },
-    account: {
-        password: { type: String, required: true },
-
-        otp: {
+const UserSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        avatar: {
             type: String,
-            default: null,
-        },
-
-        otpExp: {
-            type: Date,
-            default: null,
-        },
-        otpLeft: {
-            type: Number,
-            default: 5,
-        },
-<<<<<<< HEAD
-
-            isVerified: {
-                type: Boolean,
-                default: false,
-            },
-            loginType: {
-                type: String,
-                enum: ["GOOGLE", "SYSTEM"], // Sử dụng enum để giới hạn các giá trị hợp lệ
-                required: true,
-=======
-        account: {
-            type: {
-                password: { type: String, required: true },
-                otp: {
-                    type: String,
-                    default: null,
-                },
-                otpExp: {
-                    type: Date,
-                    default: null,
-                },
-                otpWrongCount: {
-                    type: Number,
-                    default: 0,
-                },
-                isVerified: {
-                    type: Boolean,
-                    default: false,
-                },
-                loginType: {
-                    type: String,
-                    enum: ["GOOGLE", "SYSTEM"], // Sử dụng enum để giới hạn các giá trị hợp lệ
-                    required: true,
-                },
->>>>>>> refs/remotes/origin/main
-            },
             required: true,
+            default:
+                "https://cellphones.com.vn/sforum/wp-content/uploads/2023/10/avatar-trang-4.jpg",
+        },
+        account: {
+            password: { type: String, required: true },
+
+            otp: {
+                type: String,
+                default: null,
+            },
+
+            otpExp: {
+                type: Date,
+                default: null,
+            },
+            otpLeft: {
+                type: Number,
+                default: 5,
+            },
+            account: {
+                type: {
+                    password: { type: String, required: true },
+                    otp: {
+                        type: String,
+                        default: null,
+                    },
+                    otpExp: {
+                        type: Date,
+                        default: null,
+                    },
+                    otpWrongCount: {
+                        type: Number,
+                        default: 0,
+                    },
+                    isVerified: {
+                        type: Boolean,
+                        default: false,
+                    },
+                    loginType: {
+                        type: String,
+                        enum: ["GOOGLE", "SYSTEM"], // Sử dụng enum để giới hạn các giá trị hợp lệ
+                        required: true,
+                    },
+                },
+                required: true,
+            },
         },
     },
     modelOption
--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,19 +1,27 @@
 import { body } from "express-validator";
 import userModel from "../models/user.model";
 const validateLogin = [
-    body("email").isEmail().withMessage("Email không hợp lệ"),
+    body("email")
+        .notEmpty()
+        .withMessage("Thiếu trường email")
+        .isEmail()
+        .withMessage("Email không hợp lệ"),
     body("password")
+        .notEmpty()
+        .withMessage("Thiếu trường password")
         .isLength({ min: 6 })
         .withMessage("Mật khẩu phải có ít nhất 6 ký tự"),
 ];
 
 const validateRegister = [
     body("email")
+        .notEmpty()
+        .withMessage("thiếu trường email")
         .isEmail()
         .withMessage("Email không hợp lệ")
         .custom(async (value) => {
             const user = await userModel.findOne({ email: value });
-      
+
             if (user) throw "Đã tồn tại người dùng, hãy dùng email khác!";
         }),
     body("password")
