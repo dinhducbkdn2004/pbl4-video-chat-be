13f955cc614e080920dc1aa1e253c6565c0bf4f9
TranNguyenTam <trannguyentam2004@gmail.com>
2024-09-05 07:38:39 +0700

    merge branch

--- src/controllers/auth/services/register.ts
+++ src/controllers/auth/services/register.ts
@@ -5,49 +5,32 @@ import sendMail from "../../../helpers/sendMail";
 import userModel from "../../../models/user.model";
 
 const register = async ({
-    email,
-    password,
-    name,
+  email,
+  password,
+  name,
 }: {
-    email: string;
-    password: string;
-    name: string;
+  email: string;
+  password: string;
+  name: string;
 }): Promise<string> => {
-    const checkUser = await userModel.findOne({ email });
-    if (checkUser) throw "User đã tồn tại";
+  const checkUser = await userModel.findOne({ email });
+  if (checkUser) throw "User đã tồn tại";
 
-    const otp = generateRandomNumberString(6);
-    await sendMail([email], "Mã xác thực tài khoản", OtpForm(otp));
+  const otp = generateRandomNumberString(6);
+  await sendMail([email], "Mã xác thực tài khoản", OtpForm(otp));
 
-    await userModel.create({
-        name,
-        email,
-        account: {
-            password: await hashPassword(password),
-            isVerified: false,
-            otp: otp,
-            otpExp: new Date(Date.now() + 5 * 60 * 1000),
-            loginType: "SYSTEM",
-        },
-    });
-<<<<<<< HEAD
-
-    return {
-      success: true,
-      message:
-        "Đăng ký thành công! Vui lòng kiểm tra email để xác thực tài khoản.",
-      email,
-    };
-    â;
-  } catch (error) {
-    return {
-      success: false,
-      message: `Có lỗi xảy ra trong quá trình đăng ký: ${error}`,
-    };
-  }
-=======
-    return email;
->>>>>>> c1ed574ae2b0668d4c6f70f8fa7c7eda8995dfc3
+  await userModel.create({
+    name,
+    email,
+    account: {
+      password: await hashPassword(password),
+      isVerified: false,
+      otp: otp,
+      otpExp: new Date(Date.now() + 5 * 60 * 1000),
+      loginType: "SYSTEM",
+    },
+  });
+  return email;
 };
 
 export default register;
