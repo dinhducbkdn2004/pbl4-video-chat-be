4b5a34c20ea71839ac8c7c2347de6f1f532384f6
TranNguyenTam <trannguyentam2004@gmail.com>
2024-09-07 15:07:20 +0700

    commit

--- src/controllers/auth/services/auth.service.ts
+++ src/controllers/auth/services/auth.service.ts
@@ -1,18 +1,16 @@
 import changePassword from "./changePassword";
 import checkOtp from "./checkOtp";
 import forgotPassword from "./forgotPassword";
-import getFriends from "./friend";
 import login from "./login";
 import loginByGoogle from "./loginByGoogle";
 import register from "./register";
 import resetPassword from "./resetPassword";
 import resetToken from "./resetToken";
-import searchUsersAndGroups from "./search";
+import searchUsersAndGroups from "../../search/services/search";
 import sendOtp from "./sendOtp";
 
 const authService = {
   searchUsersAndGroups,
-  getFriends,
   register,
   login,
   resetToken,
