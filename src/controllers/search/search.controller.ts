4b8d3060f9985c96716b3f59372faaacb0a2a1d0
TranNguyenTam <trannguyentam2004@gmail.com>
2024-09-14 20:00:16 +0700

    chatroom

--- src/controllers/search/search.controller.ts
+++ src/controllers/search/search.controller.ts
@@ -2,20 +2,39 @@ import { Request, Response } from 'express';
 import responseHandler from '../../handlers/response.handler';
 import { Router } from 'express';
 import { authenticate } from '../../middlewares/auth.middleware';
-import { searchChatroom, SearchUsers } from './dtos/search.dto';
-import searchUsers from './services/searchUsers';
 import searchService from './services/search.service';
+import { JwtPayload } from 'jsonwebtoken';
 
 const searchRoute: Router = Router();
 searchRoute.get('/chatrooms', authenticate, async (req: Request, res: Response) => {
-    const { name, page, limit } = req.query as {
-        name: string;
-        page: string;
-        limit: string;
-    };
-    const result = await searchService.searchChatRooms(name, Number(page), Number(limit));
+    try {
+        const {
+            name,
+            page = '1',
+            limit = '10',
+            typeRoom,
+            getMy
+        } = req.query as {
+            name: string;
+            page: string;
+            limit: string;
+            typeRoom?: 'PUBLIC' | 'PRIVATE';
+            getMy?: string;
+        };
+        const userId = (req.user as JwtPayload).userId as string;
 
-    responseHandler.ok(res, result, 'Tìm kiếm người dùng thành công!');
+        const result = await searchService.searchChatRooms(
+            name,
+            Number(page),
+            Number(limit),
+            typeRoom,
+            getMy === 'true',
+            userId
+        );
+        responseHandler.ok(res, result, 'Tìm kiếm chatroom thành công!');
+    } catch (error: any) {
+        responseHandler.errorOrBadRequest(res, error);
+    }
 });
 
 searchRoute.get('/users', authenticate, async (req: Request, res: Response) => {
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,36 +1,33 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
+import responseHandler from '../handlers/response.handler';
+import { verifyAccessToken } from '../helpers/jwtToken';
 
-import responseHandler from "../handlers/response.handler";
-import { verifyAccessToken } from "../helpers/jwtToken";
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers?.authorization;
 
-export const authenticate = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const authHeader = req.headers?.authorization;
-
-  if (!authHeader) {
-    return responseHandler.unauthenticate(res);
-  }
-
-  const token = authHeader.startsWith("Bearer ")
-    ? authHeader.slice(7)
-    : authHeader;
-
-  try {
-    const decode = verifyAccessToken(token);
-
-    (req as any).user = typeof decode === "string" ? decode : decode.data;
-
-    next();
-  } catch (error: any) {
-    console.log(error);
-
-    if (error.message?.includes("jwt expired")) {
-      responseHandler.accessTokenExpired(res);
-      return;
+    if (!authHeader) {
+        return responseHandler.unauthenticate(res);
     }
-    responseHandler.error(res, error);
-  }
+
+    const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : authHeader;
+
+    try {
+        const decoded = verifyAccessToken(token);
+        if (typeof decoded === 'string') {
+            req.user = decoded;
+        } else if (typeof decoded === 'object' && decoded.data) {
+            req.user = decoded.data;
+        }
+
+        next();
+    } catch (error: any) {
+        console.log(error);
+
+        if (error.message?.includes('jwt expired')) {
+            responseHandler.accessTokenExpired(res);
+            return;
+        }
+
+        responseHandler.error(res, error);
+    }
 };
