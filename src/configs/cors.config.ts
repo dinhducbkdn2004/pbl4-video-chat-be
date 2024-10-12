c4541a681c6dc3b6a97b498feee5ef0508dc1f8a
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2024-10-12 14:34:25 +0700

    hotfix: cors

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -1,7 +1,12 @@
 import { CorsOptions } from 'cors'
 import { log } from 'console'
 
-const whitelist = ['http://localhost:5173', 'https://pbl4-video-chat-fe.onrender.com', 'http://192.168.64.1:5173']
+const whitelist = [
+    'http://localhost:5173',
+    'https://pbl4-video-chat-fe.onrender.com',
+    'http://192.168.64.1:5173',
+    'http://172.20.10.13:5173'
+]
 const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
         if (!origin || whitelist.includes(origin)) {
