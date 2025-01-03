3abac383c5b12dccafa0d505eac2edb25d87dc30
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2025-01-03 10:19:36 +0700

    add cor

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -5,7 +5,8 @@ const whitelist = [
     'http://localhost:5173',
     'https://pbl4-video-chat-fe.onrender.com',
     'http://192.168.64.1:5173',
-    'http://172.20.10.13:5173'
+    'http://172.20.10.13:5173',
+    'https://phuocnguyn.id.vn'
 ]
 const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
