bee8e99c267ec2ad4835fea209b009e2e766b466
Trần Nguyên Tâm <trannguyentam2004@gmail.com>
2025-01-04 18:02:41 +0700

    fix leave

--- src/api/v1/chat-room/services/group/leaveGroup.ts
+++ src/api/v1/chat-room/services/group/leaveGroup.ts
@@ -12,21 +12,20 @@ const leaveGroup = async (chatRoomId: string, userId: string) => {
     if (!chatRoom.participants.includes(new Types.ObjectId(userId))) {
         throw new Error('Bạn không nằm trong Groupchat!')
     }
-    
+
     const user = await userModel.findById(userId)
     if (!user) {
         throw new Error('User không tồn tại!')
     }
 
     const isAdmin = chatRoom.admins.includes(new Types.ObjectId(userId))
-
-    if (isAdmin && chatRoom.admins.length == 1) {
+    if (isAdmin && chatRoom.admins.length === 1) {
         throw new Error('Phải bổ nhiệm Admin khác cho Group trước khi rời nhóm!')
     }
 
-    if (chatRoom.participants)
-        chatRoom.participants = chatRoom.participants.filter((participant) => !participant.equals(userId))
+    chatRoom.participants = chatRoom.participants.filter((participant) => !participant.equals(userId))
     await chatRoom.save()
+
     await notificationService.createNotification('Bạn đã rời khỏi cuộc trò chuyện', userId, 'ChatRooms', chatRoomId)
 
     const remainingMembers = chatRoom.participants
@@ -35,6 +34,8 @@ const leaveGroup = async (chatRoomId: string, userId: string) => {
     for (const member of remainingMembers) {
         await notificationService.createNotification(message, member.toString(), 'ChatRooms', chatRoomId)
     }
+
     return chatRoom
 }
+
 export default leaveGroup
