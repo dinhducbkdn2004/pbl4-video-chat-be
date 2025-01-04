84908c7ac692c582bba933bcf69f392da48e3cfe
Trần Nguyên Tâm <trannguyentam2004@gmail.com>
2025-01-04 18:00:56 +0700

    fix addmember

--- src/api/v1/chat-room/services/group/addMember.ts
+++ src/api/v1/chat-room/services/group/addMember.ts
@@ -10,14 +10,17 @@ const addMember = async (chatRoomId: string, newMemberId: string, adminId: strin
 
     if (chatRoom.typeRoom !== 'Group') throw new Error('Chỉ có room nhóm mới có thể thêm thành viên!')
 
-    const isAdmin = chatRoom.admins.includes(new Types.ObjectId(adminId))
-    const isModerator = chatRoom.moderators.includes(new Types.ObjectId(adminId))
+    if (chatRoom.privacy === 'PRIVATE') {
+        const isAdmin = chatRoom.admins.includes(new Types.ObjectId(adminId))
+        const isModerator = chatRoom.moderators.includes(new Types.ObjectId(adminId))
 
-    if (!isAdmin && !isModerator) throw new Error('Chỉ có admin hoặc moderator mới có quyền thêm thành viên!')
+        if (!isAdmin && !isModerator) throw new Error('Chỉ có admin hoặc moderator mới có quyền thêm thành viên!')
+    }
 
     if (chatRoom.participants.includes(new Types.ObjectId(newMemberId))) {
         throw new Error('Thành viên đã có trong phòng chat!')
     }
+
     chatRoom.participants.push(new Types.ObjectId(newMemberId))
     await chatRoom.save()
 
