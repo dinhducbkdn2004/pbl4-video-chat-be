34555cec4f0b685e0293a42fdce91304af8d5c34
DinhDuc <dinhducbkdn2004@gmail.com>
2025-01-04 16:44:14 +0700

    Revert "hotfix:sent-request-api"
    This reverts commit b0cee192f318635acee2c0f0f76a286a85e5410e, reversing
    changes made to 7c5f782a335cb93b57b56a9026e619127f3a3133.

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -5,15 +5,13 @@ const whitelist = [
     'http://localhost:5173',
     'https://pbl4-video-chat-fe.onrender.com',
     'http://192.168.64.1:5173',
-    'http://172.20.10.13:5173'
+    'http://172.20.10.13:5173',
+    'https://phuocnguyn.id.vn'
 ]
 const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
-        if (!origin || whitelist.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+        console.log(origin)
+        callback(null, true)
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
