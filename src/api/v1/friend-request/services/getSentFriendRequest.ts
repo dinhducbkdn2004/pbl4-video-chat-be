c45b0ff84c8c9ca7e18f3ac72c66202d3fb5e4a9
Trần Nguyên Tâm <trannguyentam2004@gmail.com>
2025-01-04 16:30:54 +0700

    fix sentrequest

--- src/api/v1/friend-request/services/getSentFriendRequest.ts
+++ src/api/v1/friend-request/services/getSentFriendRequest.ts
@@ -1,13 +1,13 @@
 import userModel from '../../user/user.model'
+import friendRequestModel from '../friendRequest.model'
 
 const getSentRequest = async (userId: string) => {
-    const user = await userModel
-        .findById(userId)
-        .select('sentRequests')
-        .populate('sentRequests', 'name avatar introduction')
-    if (!user || !user.sentRequests.length) {
-        throw new Error('Không tìm thấy lời mời')
-    }
-    return user.sentRequests
+    const request = await friendRequestModel
+        .find({
+            sender: userId,
+            status: 'PENDING'
+        })
+        .populate('receiver', 'name avatar')
+    return request
 }
 export default getSentRequest
