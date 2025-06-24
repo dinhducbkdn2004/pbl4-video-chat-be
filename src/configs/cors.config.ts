2fa478057b4e45132910f0eed6b7662abc6fc6a3
Your Name <your-email@example.com>
2025-06-24 15:40:38 +0700

    hotfix: socket core config

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -6,7 +6,9 @@ const whitelist = [
     'https://pbl4-video-chat.vercel.app',
     'http://172.188.113.94:5173',
     'http://ducbkdn.space',
-    'https://ducbkdn.space'
+    'https://ducbkdn.space',
+    'http://www.ducbkdn.space',
+    'https://www.ducbkdn.space',
 ]
 const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
